1129502ad550901d6db66e6a90d89e5552ba6e04
Ed Tansey <office.fwd@yahoo.com>
2021-08-17 11:54:30 +0100

    Add spoiler project files

---
+++ spoiler/js/app.js
@@ -0,0 +1,15 @@
+$('.spoiler').on('click', 'button', function(event){
+	console.log(event.target);
+	// Show the spoiler text
+	$(this).prev().show();
+	// Hide the "Reveal Spoiler" button
+	$(this).hide();
+});
+
+// Create the "Reveal Spoiler" button
+const $button = $('<button>Reveal Spoiler</button>');
+// Append to web page
+$('.spoiler').append($button);
+
+// Hide the spoiler text
+$('.spoiler span').hide();
